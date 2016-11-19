7766265377709ed14b16956981a4f92353413f1a
Mauricio Vignale <mvignale1987@gmail.com>
2016-11-19 11:28:40 -0300

    ActionBar Icons further adjustments from title bar

--- app/src/main/java/com/mauri/android/flickrexample/activities/MainActivity.java
+++ app/src/main/java/com/mauri/android/flickrexample/activities/MainActivity.java
@@ -3,8 +3,12 @@ package com.mauri.android.flickrexample.activities;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.mauri.android.flickrexample.R;
 import com.mauri.android.flickrexample.adapters.FlickrImagesAdapter;
@@ -22,6 +26,9 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int CARD_VIEW = 1;
+    private static final int GRID_VIEW = 3;
+
     @BindView(R.id.flickr_view)
     RecyclerView mFlickrView;
 
@@ -36,14 +43,41 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.action_bar_center_title);
         ButterKnife.bind(this);
         mFlickrView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        mFlickrView.setLayoutManager(layoutManager);
+        mFlickrView.setLayoutManager(new GridLayoutManager(this, GRID_VIEW));
         FlickrExampleApp.get(this).getNetworkComponent().plus(new MainActivityModule(this)).inject(this);
         mainActivityPresenter.getRecentImages();
     }
 
-    public void loadFlickrView(List<Photo> flickrImages){
-        FlickrImagesAdapter flickrAdapter = new FlickrImagesAdapter(flickrImages,this);
+    public void loadFlickrView(List<Photo> flickrImages) {
+        FlickrImagesAdapter flickrAdapter = new FlickrImagesAdapter(flickrImages, this);
         mFlickrView.setAdapter(flickrAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_name) {
+            toggleSpanCount(item);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void toggleSpanCount(MenuItem item) {
+        int columnCount = ((GridLayoutManager) mFlickrView.getLayoutManager()).getSpanCount();
+        if (columnCount == GRID_VIEW) {
+            ((GridLayoutManager) mFlickrView.getLayoutManager()).setSpanCount(CARD_VIEW);
+            item.setIcon(R.drawable.grid_icon);
+
+        } else {
+            ((GridLayoutManager) mFlickrView.getLayoutManager()).setSpanCount(GRID_VIEW);
+            item.setIcon(R.drawable.card_icon);
+        }
     }
 }
